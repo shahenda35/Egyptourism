e2850e11a30fe6558a8277be77b77d88b049bec3
shahenda <shahendahesham@gmail.com>
2025-01-28 21:26:03 +0200

    update signup and contact us

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
-
+  email: string = "Escape@gmail.com";
 }
